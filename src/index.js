9e9d41a35c808709245b114ba3897ecb6f9b9a2c
Shrutesh Pachineela <sruteshp@gmail.com>
2020-07-30 08:52:48 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function configger(defaults, aliases) {
+    return (config) => {
+        const result = {
+            ...config,
+        };
+
+        Object.entries(aliases).forEach(([keyToBeAliased, alias]) => {
+            if (keyToBeAliased in result) {
+                if (alias in result) {
+                    // Both alias and keyToBeAlias are given
+                    delete result[keyToBeAliased];
+                    return;
+                }
+                result[alias] = result[keyToBeAliased];
+                delete result[keyToBeAliased];
+            }
+        });
+
+        Object.entries(defaults).forEach(([key, value]) => {
+            if (!(key in result)) {
+                result[key] = value;
+            }
+        });
+
+        return result;
+    };
+}
+
+export default configger;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const babelConfig = require('./babel.config');
+
+const libraryName = 'configger';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'index.js',
+        library: {
+            root: libraryName,
+            amd: libraryName,
+            commonjs: libraryName,
+        },
+        libraryTarget: 'umd',
+        // Allow common-js to require directly
+        // useAsync = require('react-use-async-hook')
+        // rather than require('react-use-async-hook').default
+        // https://github.com/webpack/webpack/issues/3929#issuecomment-423514570
+        libraryExport: 'default',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: babelConfig,
+                },
+            },
+        ],
+    },
+    devtool: 'source-map',
+};
